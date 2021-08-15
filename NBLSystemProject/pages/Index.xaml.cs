0ff8608fa9c293f9361472a91e8c9016ba73f274
Anderson Lima <anderson.lima@vibetecnologia.com>
2021-08-15 11:28:35 -0300

    Add project files.

---
+++ NBLSystemProject/pages/Index.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NBLSystemProject.Pages
+{
+
+    public partial class Index : Window
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCloseWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            if (MessageBox.Show("Saindo do Sistema!", "Atenção!!!", MessageBoxButton.OKCancel, MessageBoxImage.Warning) == MessageBoxResult.OK) {
+                this.Close();
+            }
+        }
+    }
+}
